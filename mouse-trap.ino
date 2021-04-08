c053ac98511de86ec3dfa0b82c1dcd1555109ce5
mollachouse <molla.chouse@gmail.com>
2021-04-08 20:31:15 +0200

    Add files via upload

---
+++ mouse-trap.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+Servo servo;
+int const trig = 3;
+int const echo = 2;
+
+void setup()
+{
+pinMode(trig, OUTPUT); 
+pinMode(echo, INPUT);
+servo.attach(5);
+}
+
+void loop()
+{       
+  int duration, distance;
+  digitalWrite(trig, LOW);
+  delay(2);
+  digitalWrite(trig, HIGH); 
+  delay(10);
+  digitalWrite(trig, LOW);
+  
+  duration = pulseIn(echo, HIGH);
+  distance = (duration/2) / 29.1;
+  
+  if (distance <= 8 && distance <= 0){
+    servo.write(50);
+    delay(3000);
+    }
+    else {
+      servo.write(160);
+      }
+      delay(30);
+      }
